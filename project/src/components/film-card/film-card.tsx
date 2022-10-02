e4d6b278ddc51b0b7c1209a0591c9e2330aa228e
iulpv <yul_karpova@mail.ru>
2022-10-02 14:00:40 +0500

    add components

---
+++ project/src/components/film-card/film-card.tsx
@@ -0,0 +1,20 @@
+type FilmCardProps = {
+  alt: string;
+  src: string;
+  name: string;
+}
+
+function FilmCard({alt, src, name}: FilmCardProps): JSX.Element {
+  return (
+    <article className="small-film-card catalog__films-card">
+      <div className="small-film-card__image">
+        <img src={src} alt={alt} width='280' height='175'/>
+      </div>
+      <h3 className="small-film-card__title">
+        <a className="small-film-card__link" href="film-page.html">{name}</a>
+      </h3>
+    </article>
+  );
+}
+
+export default FilmCard;
