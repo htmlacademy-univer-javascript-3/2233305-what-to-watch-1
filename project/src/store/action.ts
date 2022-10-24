1f1b8b1f01be4dc62293815cbcc8e3a04c554eea
iulpv <yul_karpova@mail.ru>
2022-10-24 16:28:03 +0500

    add redux

---
+++ project/src/store/action.ts
@@ -0,0 +1,7 @@
+import {createAction} from '@reduxjs/toolkit';
+
+export const changeGenre = createAction('films/changeGenre' ,(value) => ({
+  payload: value,
+}));
+
+export const getFilms = createAction('films/getFilms');
