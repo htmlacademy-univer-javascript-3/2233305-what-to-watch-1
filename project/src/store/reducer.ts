cd1bdf7d7e0348ff399b9e0c600cbf2bcefba1de
iulpv <yul_karpova@mail.ru>
2022-10-24 17:07:01 +0500

    add show more functional

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,12 +1,15 @@
 import {createReducer} from '@reduxjs/toolkit';
-import {changeGenre, getFilms} from './action';
+import {changeGenre, getFilms, resetFilmsCount, showMore} from './action';
 import {films} from '../mocks/film';
 
-const INITIAL_STATE = 'All genres';
+const INITIAL_STATE_GENRE = 'All genres';
+const INITIAL_STATE_FILM_COUNT = 8;
+const ALL_FILMS = 8;
 
 const initialState = {
-  genre: INITIAL_STATE,
+  genre: INITIAL_STATE_GENRE,
   filmsGenre: films,
+  filmsCount : INITIAL_STATE_FILM_COUNT
 };
 
 const reducer = createReducer(initialState, (builder) => {
@@ -16,9 +19,16 @@ const reducer = createReducer(initialState, (builder) => {
     });
   builder
     .addCase(getFilms, (state) => {
-      state.filmsGenre = state.genre === INITIAL_STATE ? films : films.filter((film) => film.genre === state.genre);
+      state.filmsGenre = state.genre === INITIAL_STATE_GENRE ? films : films.filter((film) => film.genre === state.genre);
     });
-
+  builder
+    .addCase(showMore, (state) => {
+      state.filmsCount = ALL_FILMS > state.filmsCount ? state.filmsCount + 8 : state.filmsCount;
+    });
+  builder
+    .addCase(resetFilmsCount, (state) => {
+      state.filmsCount = INITIAL_STATE_FILM_COUNT;
+    });
 });
 
 export {reducer};
