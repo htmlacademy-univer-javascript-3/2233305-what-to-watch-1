68885019e44fa18a42512a245ede20757f85b9fc
iulpv <yul_karpova@mail.ru>
2022-12-15 23:36:08 +0500

    main page fix

--- project/src/pages/main-screen/main-screen.tsx
+++ project/src/pages/main-screen/main-screen.tsx
@@ -3,8 +3,7 @@ import Logo from '../../components/logo/logo';
 import Footer from '../../components/footer/footer';
 import CatalogGenres from '../../components/main/catalog-genres/catalog-genres';
 import {Link, useNavigate} from 'react-router-dom';
-import {useEffect, useState} from 'react';
-import {Film} from '../../types/types';
+import {useEffect} from 'react';
 import {useAppDispatch, useAppSelector} from '../../hooks';
 import {changeGenre, resetCount, showMore} from '../../store/action';
 import User from "../../components/user/user";
@@ -16,27 +15,32 @@ import {
   getPromoFilm
 } from "../../store/films-data/selectors";
 import {INITIAL_STATE_GENRE} from "../../const";
+import NotFound from "../not-found/not-found";
 
 
 function MainScreen(): JSX.Element {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
+
   useEffect(() => {
     dispatch(changeGenre(INITIAL_STATE_GENRE))
     dispatch(resetCount())
   }, [dispatch])
 
+  const promoFilm = useAppSelector(getPromoFilm);
   const genre = useAppSelector(getGenre);
   const filmsCount = useAppSelector(getFilmsCount);
-  const promoFilm = useAppSelector(getPromoFilm);
   const favoriteFilms = useAppSelector(getFavoriteFilms);
   const genresFilm = useAppSelector(getFilmsWithGenre)
-  const [,setEnter] = useState<Film | null>(null);
+
+  if (promoFilm === undefined)
+    return <NotFound/>
+
   return (
     <>
       <section className="film-card">
         <div className="film-card__bg">
-          <img src={promoFilm?.backgroundImage} alt={promoFilm?.name}/>
+          <img src={promoFilm.backgroundImage} alt={promoFilm.name}/>
         </div>
 
         <h1 className="visually-hidden">WTW</h1>
@@ -49,26 +53,26 @@ function MainScreen(): JSX.Element {
         <div className="film-card__wrap">
           <div className="film-card__info">
             <div className="film-card__poster">
-              <img src={promoFilm?.posterImage} alt={promoFilm?.name}
+              <img src={promoFilm.posterImage} alt={promoFilm.name}
                    width="218" height="327"
               />
             </div>
 
             <div className="film-card__desc">
-              <h2 className="film-card__title">{promoFilm?.name}</h2>
+              <h2 className="film-card__title">{promoFilm.name}</h2>
               <p className="film-card__meta">
-                <span className="film-card__genre">{promoFilm?.genre}</span>
-                <span className="film-card__year">{promoFilm?.released}</span>
+                <span className="film-card__genre">{promoFilm.genre}</span>
+                <span className="film-card__year">{promoFilm.released}</span>
               </p>
 
               <div className="film-card__buttons">
-                <Link to={`/player/${promoFilm?.id}`} className="btn btn--play film-card__button" type="button"
->
+                <Link to={`/player/${promoFilm.id}`} className="btn btn--play film-card__button" type="button"
+                >
                   <svg viewBox="0 0 19 19" width="19" height="19">
                   </svg>
                   <span>Play</span>
                 </Link>
-                <Link to={'/mylist'} className="btn btn--list film-card__button" type="button" >
+                <Link to={'/mylist'} className="btn btn--list film-card__button" type="button">
                   <svg viewBox="0 0 19 20" width="19" height="20">
                   </svg>
                   <span>My list</span>
@@ -86,10 +90,7 @@ function MainScreen(): JSX.Element {
           <CatalogGenres genre={genre}/>
           <div className="catalog__films-list">
             {genresFilm.slice(0, filmsCount).map((film) => (
-              <FilmCard key={film.id} film={film} onMouseEnter={() => {
-                setEnter(film);
-              }}
-                        onMouseLeave={() => setEnter(null)} onClick={() => {
+              <FilmCard key={film.id} film={film} onClick={() => {
                 navigate(`/films/${film.id}`);
               }}
               />))}
